d8194eeb597dabb5f17721292e1ec218c237a23e
GajananBodhankar <bodhankargajanan99@gmail.com>
2023-09-10 20:21:57 +0530

    Initiated news app project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Navbar from "./Components/navbar";
+
+export class App extends Component {
+  render() {
+    return <Navbar />;
+  }
+}
+
+export default App;
